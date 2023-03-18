84d2ee983cbeedadc1009a5de779fd09a1f8a821
lasha <jimshitashvili4444@gmail.com>
2023-03-19 00:31:14 +0400

    annualPlan added

--- script.js
+++ script.js
@@ -2,31 +2,51 @@ const button = document.querySelector('.switch');
 const range = document.querySelector('#range');
 const views = document.querySelector('#pageviews');
 const price = document.querySelector('.cost');
+const active = document.querySelector('.active');
+const month = document.querySelector('.month');
+let cost;
 
+function annualConverter(){
+    if(button.classList.contains('active')){
+        month.textContent = "/ year"
+        cost = cost*12*85/100;
+        return cost;
+        
+    }
+}
 
-
-function ranges(){
+function monthly(){
     if(range.value < 100){
+        cost = 8;
+        annualConverter();
         views.textContent = "10K pageviews";
-        price.textContent = "$8.00";
+        price.textContent = "$" + cost.toFixed(2);
     }else if(range.value >= 100 && range.value < 200){
+        cost = 12;
+        annualConverter();
         views.textContent = "50K pageviews";
-        price.textContent = "$12.00";
+        price.textContent = "$" + cost.toFixed(2);
     }else if(range.value >= 200 && range.value < 300){
+        cost = 16;
+        annualConverter();
         views.textContent = "100K pageviews";
-        price.textContent = "$16.00";
+        price.textContent = "$" + cost.toFixed(2);
     }else if(range.value >= 300 && range.value < 400){
+        cost = 24;
+        annualConverter();
         views.textContent = "500K pageviews";
-        price.textContent = "$24.00";
+        price.textContent = "$" + cost.toFixed(2);
     }else if(range.value >= 400 && range.value <= 500){
+        cost = 36;
+        annualConverter();
         views.textContent = "1M pageviews";
-        price.textContent = "$36.00";
+        price.textContent = "$" + cost.toFixed(2);
     }
 }
 
 
 
-range.addEventListener('input', ranges);
+range.addEventListener('input', monthly);
 
 button.addEventListener('click', () => {
     button.classList.toggle('active');
